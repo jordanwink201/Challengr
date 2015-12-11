0e47f062fb10e273a4daaabfd60785eded8a5658
arthur <arthur.mathies@googlemail.com>
2015-12-11 14:48:14 -0800

    [feat]: switching to remote database

--- database/database.js
+++ database/database.js
@@ -10,21 +10,20 @@ var sequelize = require('sequelize');
 
 // instantiates a new Sequelize database
 var db = new sequelize(
-  // database username and password do not matter for sqlite
-  'database',
-  'username',
-  'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    // the pool size depends on the load the database has to handle
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD, {
+    host: process.env.DATABASE_URL,
+    port: process.env.DATABASE_PORT,
+    dialect: 'mysql',
     pool: {
       max: 10,
       min: 2,
       idle: 100000
     },
-    storage: __dirname + '/data/db.sqlite',
-    logging: false
+    logging : false
   });
+
 require(__dirname + '/configDatabase.js')(db);
 
 module.exports = db;
