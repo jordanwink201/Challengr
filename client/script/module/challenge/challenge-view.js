e99ae0c8f1f3812a6d73ec3497da425f8b0ba8be
Jordan Winkelman <jordanwink201@gmail.com>
2015-12-13 00:41:15 -0800

    [feat]: challenge view detail shows remaining time in countdown form if active

--- client/script/module/challenge/challenge-view.js
+++ client/script/module/challenge/challenge-view.js
@@ -7,12 +7,18 @@ Challenges in newsfeed view
 
 angular.module('App.challengeView', [])
 
-.controller('challengeViewCtrl', ['challengeFactory', '$stateParams', '$state', 'challengeService', 'alertService', '$rootScope', function (challengeFactory, $stateParams, $state, challengeService, alertService, $rootScope) {
+.controller('challengeViewCtrl', ['challengeFactory', '$stateParams', '$state', 'challengeService', 'alertService', '$rootScope', '$timeout', function (challengeFactory, $stateParams, $state, challengeService, alertService, $rootScope, $timeout) {
 
   var self = this;
   self.challenge = null;
+  self.remaining = null;
   self.isCurrentUserChallenge = false;
   var currentUserEmail = localStorage.getItem('com.challengr.email');
+
+  self.configureDirective = function () {
+    self.readChallenge();
+    createCountdown();
+  };
 
   // Read the challenge id from the service object to retreive the correct challenge by it's ID
   self.readChallenge = function () {
@@ -58,5 +64,48 @@ angular.module('App.challengeView', [])
         console.log('error updating completion status');
       });
   };
+
+  function checkCompletedStatus() {
+    var issue = moment(self.challenge.issuedDate);
+    var now = moment();
+    var difference = now.diff(issue, 'hours');
+
+    if (difference >= 1) {
+      if (self.challenge.notCompleted === false && self.challenge.completed === false) {
+        // set the challenge to be completed
+        self.challenge.notCompleted = true;
+
+        var updateObj = {
+          id: self.challenge.id,
+          notCompleted: true
+        };
+        // call factory function to update challenge values
+        challengeFactory.updateChallenge(updateObj)
+          .catch(function (err) {
+            console.log('error changing status to completed : ', err);
+          });
+      }
+    }
+  }
+
+  function createCountdown() {
+    console.log('challenge : ', self.challenge);
+    var expire = moment(self.challenge.expiresDate);
+    var now = moment();
+    var interval = -1;
+    var counter = moment.duration(expire.diff(now), 'ms');
+
+    (function tick() {
+      counter = moment.duration(counter.asMinutes() + interval, 'minutes');
+      if (Math.floor(counter.minutes()) === 0) {
+        checkCompletedStatus();
+      } else {
+        var minutes = counter.minutes();
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        self.remaining = counter.hours() + 'h ' + minutes + ' m';
+        countdownTimeout = $timeout(tick, 60000);
+      }
+    })();
+  }
 
 }]);
